6ac472bad4e788fc95d2bbc7b1ff54e7562261f8
Spencer Ng <sng1488@gmail.com>
2021-10-13 03:38:32 -0500

    Add overlay and css injection

---
+++ hw2/README.md
@@ -0,0 +1,7 @@
+# (Tracking)Pixels
+
+A browser extension to visualize tracking pixels and scripts collected across various websites
+
+## Installation
+
+From your favorite browser, select "Load Unpacked Extension" and select the folder that this README (along with `manifest.json`) is extracted to. For optimal results, use this extension on the new Reddit while logged into a Facebook or Instagram account.
--- hw2/tracking-pixels.js
+++ hw2/tracking-pixels.js
@@ -37,14 +37,12 @@ function getTrackingScripts() {
 }
 
 initializeStorage();
-storeTrackingScripts();
-
 
 
 function initializeStorage() {
     console.log("Getting past trackers")
-    pastTrackers = window.localStorage.getItem("pastTrackers")
-    displayedPixels = window.localStorage.getItem("displayedPixels")
+    pastTrackers = storage.getItem("pastTrackers")
+    displayedPixels = storage.getItem("displayedPixels")
     if (pastTrackers == undefined) {
         pastTrackers = {
             "google": [],
@@ -58,23 +56,36 @@ function initializeStorage() {
             "linkedin": [],
             "reddit": []
         }
-        window.localStorage.setItem("pastTrackers", pastTrackers)
-    }
-
+        storage.setItem("pastTrackers", pastTrackers)
+    } 
+    console.log(pastTrackers["facebook"]);
+    storeTrackingScripts();
 }
+
+var pixSize = 10;
 
 function addSite(siteInfo, company) {
     pastTrackers[company].push(siteInfo);
-    window.localStorage.setItem("pastTrackers", pastTrackers)
-    // TODO: add pixel display/generation here such that it's stored
-    // in displayedPixels
-    // Pixel specified by color (based on company name), location s.t. no overlap
+    
+    var companyColors = {
+        "google": "#0F9D58",
+        "facebook": "#3b5998",
+        "linkedin": "0077b5",
+        "reddit": "#ff4301"
+    }
+
+    var x = Math.round(Math.random() * document.documentElement.clientWidth - pixSize);
+    var y = Math.round(Math.random() * document.documentElement.clientHeight - pixSize);
+    var color = companyColors[company] // todo: make this randomized
+    
+    displayedPixels[company].push([x, y, color])
 }
 
 function storeTrackingScripts() {
     var siteInfo = {
         "url": window.location.href,
         "title": document.title,
+        "site": document.querySelector("meta[property='og:site_name']").getAttribute('content'),
         "image": document.querySelector("meta[property='og:image']").getAttribute('content'),
         "description": document.querySelector("meta[property='og:description']").getAttribute('content')
     }
@@ -82,12 +93,41 @@ function storeTrackingScripts() {
     for (const company of getTrackingScripts()) {
         addSite(siteInfo, company);
     }
+    
+    storage.setItem("pastTrackers", pastTrackers)
+    storage.setItem("displayedPixels", displayedPixels)
 
     displayPixels(displayedPixels);
 }
 
+// display pixels and make them expandable
 function displayPixels(pixels) {
-    // display pixels and make them expandable
+    var overlay = "<div id=\"pixelOverlay\"></div>"
+
+    var head  = document.getElementsByTagName('head')[0];
+    var cssElement = "<link rel=\"stylesheet\" type=\"text/css\" href=\"tracking-pixels.css\" media=\"screen\"/>"
+
+    head.innerHTML += cssElement;
+
+    var body  = document.getElementsByTagName('body')[0];
+    body.innerHTML = overlay + body.innerHTML;
+
+    var pixelOverlay = document.getElementById("pixelOverlay")
+
+    for (const [company, companyPixels] of pixels) {
+        for (var i = 0; i < companyPixels.length; i++) {
+           var x = companyPixels[i][0]
+           var y = companyPixels[i][1]
+           var color = companyPixels[i][2]
+
+           var pixel = pixelOverlay.createElement("div");
+           pixel.style.left = x + "px";
+           pixel.style.top = y + "px";
+           pixel.style.height = pixSize + "px";
+           pixel.style.width = pixSize + "px";
+           pixel.style.backgroundColor = color;
+        }
+    }
 }
 
 function injectAds() {
